c0218f6667dbbf01084d4fd237b577601ec052ae
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-02-16 18:36:19 -0800

    Finish sidebar along with css & content section

---
+++ src/components/contentSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "ABOUT BLOCCHITECH",
+  headLine: "WE FOCUS ON SUSTAINABLE TECHNOLOGY",
+  description:
+    "Here at Blocchitech we place great emphasis on finding ways to provide value to our investors while maintaining sustainable high performing data centers.  ",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/server.svg"),
+  alt: "server",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "whatwedo",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "WHAT WE DO",
+  headLine: "MINING FROM ESTABLISHED JURISDICTIONS",
+  description:
+    "Located in North America, the combination of hydropower highly-efficient data centers, proprietary cooling technologies, and long-term partnerships has allowed BlocchiTech to maximize it's profits.",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: require("../../images/partnership.svg"),
+  alt: "partnerships",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "latestnews",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "LATEST NEWS",
+  headLine: "INVESTORS LINE UP AS BLOCCHITECH WELCOMED IN TEXAS",
+  description:
+    "BlocchiTech A Cryptocurrency Mining Company Open In North America, Counties Are Welcoming Them, and Investors Want To Get In - 02/02/23",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/investors.svg"),
+  alt: "investors",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
